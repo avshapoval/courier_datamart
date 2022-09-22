f2618596dbbf62c3d1ffc0a3cfa7fd209f149398
Aleksandr Shapoval <a-v.shapowal@yandex.ru>
2022-09-22 14:37:02 +0300

    Update dds_to_cdm_dag.py

--- src/dags/dds_to_cdm_dag.py
+++ src/dags/dds_to_cdm_dag.py
@@ -1,9 +1,13 @@
 import pendulum
-
+import json
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.postgres_operator import PostgresOperator
+from airflow.models import Variable
+
+string_headers = Variable.get("CONNECTION_HEADERS")
+headers = json.loads(string_headers)
 
 pg_warehouse_conn_id = 'PG_WAREHOUSE_CONNECTION'
 CUR_DATE = '{{ ts }}'
